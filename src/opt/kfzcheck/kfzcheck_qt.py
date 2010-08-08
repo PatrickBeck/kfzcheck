a511801c35edf24ea182a78b899af0a2adfe26f3
Patrick Beck <pbeck@yourse.de>
2010-08-08 03:35:22 +0200

    Country selector added (with ini file for the last selected country), About dialog added, a few bugs fixed :)

--- src/opt/kfzcheck/kfzcheck_qt.py
+++ src/opt/kfzcheck/kfzcheck_qt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 '''
 Filename: kfzcheck_qt.py
-Version: 0.0.1
-last change: 2010-08-1
+Version: 0.1
+last change: 2010-08-8
 Function: KFZcheck is a small program written in python and uses the Qt toolkit. 
 It searches for car (in german kfz) license plates shortcuts and the citys according to the 
 searchword - german example: S for Stuttgart. 
@@ -40,6 +41,7 @@ import csv
 import re
 import os
 import gettext
+import ConfigParser
 from gui import Ui_KFZcheck
 
 APP = 'kfzcheck' # get i18n support - english and german
@@ -52,44 +54,74 @@ _ = gettext.gettext
 class KFZcheck(QMainWindow):
 
     kfzcheck_dir = '/home/pbeck/kfzcheck_devel/src/opt/kfzcheck/'
-    kfzcheck_ini = os.path.expanduser('~/.kfzcheck.ini') # configuration file for the country selection
     listfield_list = []
-    country = 'de' # has to be replaced with a gui
+
+    Config = ConfigParser.ConfigParser()
+    kfzcheck_ini = os.path.expanduser('~/.kfzcheck.ini') # configuration file for the country selection
+    if os.path.isfile(kfzcheck_ini) == False: # when the ini file not exists create it in ~./.kfzcheck.ini
+        ini_file = open(kfzcheck_ini, 'w') # open the file and create it when not exists
+        Config.add_section('Country')
+        Config.set('Country', 'firstload', 'de') # first standard load is the german file
+        Config.write(ini_file)
+        ini_file.close()
+
+    Config.read(kfzcheck_ini)
+    firstload = Config.get('Country', 'firstload') # load the country that specified in the configuration file
 
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.ui = Ui_KFZcheck() # load the qt-designer generated gui file
         self.ui.setupUi(self)
-        QObject.connect(self.ui.searchfield, SIGNAL('textChanged(QString)'), self.searching) # call the searching function on every text change - on the fly search
+        
+        self.countryfield = QListWidget() # for the country selector
         
         self.qa = QActionGroup(None)
         self.qa.addAction(self.ui.actionTop)
         self.qa.addAction(self.ui.actionMiddle)
         self.qa.addAction(self.ui.actionBottom)
+
+        QObject.connect(self.ui.searchfield, SIGNAL('textChanged(QString)'), self.searching) # call the searching function on every text change - on the fly search
         QObject.connect(self.ui.actionTop, SIGNAL('triggered()'), self.filterTop)
         QObject.connect(self.ui.actionMiddle, SIGNAL('triggered()'), self.filterMiddle)
         QObject.connect(self.ui.actionBottom, SIGNAL('triggered()'), self.filterBottom)
+        QObject.connect(self.ui.actionAbout, SIGNAL('triggered()'), self.about)
+        QObject.connect(self.ui.actionCountry, SIGNAL('triggered()'), self.countrySelector)
+        QObject.connect(self.countryfield, SIGNAL('itemActivated(QListWidgetItem*)'), self.loadCountry)
 
+        self.load(self.firstload)
 
     def load(self, kfzlist):
         csvfile = '%s/kfzlist/%s.csv' % (self.kfzcheck_dir, kfzlist) 
         try:
             file = csv.reader(open(csvfile), delimiter=',') # parse the csv file
-        except:
-            self.addItemstoList('No csv file found')
         
-        for i in file:
-            i[0] = unicode(i[0], 'utf-8') # listfield_list is unicode, too. When i append the item.
-            i[1] = unicode(i[1], 'utf-8')
+            for i in file:
+                i[0] = unicode(i[0], 'utf-8') # listfield_list is unicode, too. When i append the item.
+                i[1] = unicode(i[1], 'utf-8')
 
-            if len(i) == 3: # check if the csv file has 3 values
-                i[2] = unicode(i[2], 'utf-8')
-                self.listfield_list.append(i)
-                self.addItemstoList('%s, %s\n%s' % (i[0], i[1], i[2]))
-            else:
-                self.listfield_list.append(i)
-                self.addItemstoList('%s, %s' % (i[0], i[1])) # when only 2 values in the csv files
-                
+                if len(i) == 3: # check if the csv file has 3 values
+                    i[2] = unicode(i[2], 'utf-8')
+                    self.listfield_list.append(i)
+                    self.addItemstoList('%s, %s\n%s' % (i[0], i[1], i[2]))
+                else:
+                    self.listfield_list.append(i)
+                    self.addItemstoList('%s, %s' % (i[0], i[1])) # when only 2 values in the csv files
+        except:
+            self.addItemstoList('csv file not found or has not enough rights.')
+            self.ui.searchfield.hide() # no searchoption - the message will be stay on the screen
+    
+    def loadCountry(self):
+        self.listfield_list = [] # clear the searching list from the old entries
+        self.ui.listfield.clear() # clear the ui from the old entries
+        selected_country = self.countryfield.currentItem().text()
+
+        self.Config.set('Country', 'firstload', selected_country) # set the config file to the selected country
+        ini_file = open(self.kfzcheck_ini, 'w') # open the file
+        self.Config.write(ini_file) # write and save it
+        ini_file.close # close the ini file
+
+        self.load(selected_country)
+            
     def searching(self):
         self.ui.listfield.clear()
         text = str(self.ui.searchfield.text())
@@ -126,24 +158,51 @@ class KFZcheck(QMainWindow):
 
         if len(textToAddList) == 2: # when two lines - the state is always on a newline - check if a image exists - it's not a problem when no image exists
             textIcon = textToAddList[1] # get the name of the image
-            icon = QIcon('%s/wappen/%s/%s.png' % (self.kfzcheck_dir, self.country, textIcon)) # set it as Qicon
+            self.Config.read(self.kfzcheck_ini)
+            secondload = self.Config.get('Country', 'firstload') # get the selected country in the moment
+            icon = QIcon('%s/wappen/%s/%s.png' % (self.kfzcheck_dir, secondload, textIcon)) # set it as QIcon
             newItem.setIcon(icon) # and add it to the newItem
         
         self.ui.listfield.addItem(newItem) # add the item to the QlistWidget
         
     def filterTop(self):
+        pass
 #        self.ui.listfield.scrollToItem()
         
     def filterMiddle(self):
+        pass
 #        self.selector.set_active(0, self.count_rows/2)
         
     def filterBottom(self):
+        pass
 #        self.selector.set_active(0, self.count_rows-1)
+
+    def about(self):
+        text = '''KFZcheck searches for kfz license plates shortcuts and citys / regions.
+ 
+Uppercase searches for all license plates starting with your search. Lowercase matches exactly for the license plates and searches after the city / region, if no license plate exists.
+
+Website: http://wiki.yourse.de/doku.php?id=python:kfzcheck
+Feedback is welcome at pbeck@yourse.de'''
+        QMessageBox.about(None, 'About', text)
+
+    def countrySelector(self):
+        country_list = []
+        path = '%s/kfzlist/' % (self.kfzcheck_dir)
+        for i in os.walk(path): # search all csv files and create a country code list
+            for i in i[2]:
+                country_list.append(i.replace('.csv', ''))
+        country_list.sort() # alphabetical sort
+        for i in country_list:
+            newCountry = QListWidgetItem()
+            newCountry.setText(i)
+            self.countryfield.addItem(newCountry)
+
+        self.countryfield.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     kfzcheck = KFZcheck()
-    kfzcheck.load('de') # has to be replaced with a gui to select
     kfzcheck.show()
     sys.exit(app.exec_())
 
