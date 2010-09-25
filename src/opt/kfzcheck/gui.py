d46f5e4376aac87562d612aa1530bdd763a4a67e
Patrick Beck <pbeck@yourse.de>
2010-09-25 23:27:58 +0200

    playing with the designer-qt

--- src/opt/kfzcheck/gui.py
+++ src/opt/kfzcheck/gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui.ui'
 #
-# Created: Thu Sep 23 20:23:26 2010
+# Created: Sat Sep 25 23:27:17 2010
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
